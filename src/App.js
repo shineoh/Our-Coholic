3fedeeec857e1937d8721351a18c64bf6d5b1ef0
kwonmoto <spadekwon@gmail.com>
2022-08-03 00:57:01 +0900

    리액트 마이그레이션 및 부트스트랩 설치, 내비바 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./component/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <p>Our-Coholic</p>
       </header>
